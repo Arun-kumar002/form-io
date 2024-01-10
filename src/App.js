1139e5d770117ab022953b981fb2869999977121
arunkumar <arunkumar.subramaniyan@ecobillz.com>
2024-01-10 16:32:55 +0530

    builder updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Render from "./components/Render";
-import { MyForm } from "./components/Builder";
+import { Builder } from "./components/Builder";
 
 export function App() {
 
@@ -12,44 +12,16 @@ export function App() {
 
   const [isSubmit, setIsSubmit] = useState(false);
 
-  const onChange = (schema) => {
 
-    if (schema?.type === "columns") {
-      let comp = JSON.parse(JSON.stringify(workingSource));
-      let allComp = [];
-      for (let v of comp?.components) {
-        if (v.key != schema.key) { allComp.push(v); }
-        else {
-          allComp.push(schema);
-        }
-      }
-      setWorkingSource({ ...workingSource, components: allComp });
-    }
-    else {
-
-      let allComp = [];
-      for (let v of schema?.components) {
-        if (v.type != "file") { allComp.push(v); }
-        else {
-          allComp.push({ type: "file", key: v.key, label: v.label, storage: "base64", image: true, fileSize: "10240KB" });
-        }
-      }
-
-      setWorkingSource({ ...schema, components: allComp });
-    }
-
-  };
 
   return (
     <div className="App">
+      {!isSubmit ? <><Builder setWorkingSource={setWorkingSource} workingSource={workingSource} /></> : <></>}
+      {isSubmit ? <Render components={workingSource} /> : <></>}
+
       <button type="button" className="btn btn-primary" onClick={() => setIsSubmit(!isSubmit)}>
         Save
       </button>
-      {!isSubmit ? <>
-        <MyForm onChange={onChange} source={workingSource} />
-      </> : <></>}
-
-      {isSubmit ? <Render components={workingSource} /> : <></>}
     </div>
   );
 }
--- src/components/Builder.jsx
+++ src/components/Builder.jsx
@@ -1,16 +1,85 @@
 import React from "react";
 import { FormBuilder } from "react-formio";
-export function MyForm(props) {
+
+export function Builder(props) {
+
+    const onChange = (schema) => {
+        if (schema?.type === "columns") {
+
+            let comp = JSON.parse(JSON.stringify(props.workingSource));
+            let allComp = [];
+            for (let v of comp?.components) {
+                if (v?.key !== schema?.key) { allComp.push(v); }
+                else {
+                    allComp.push(schema);
+                }
+            }
+            props.setWorkingSource({ ...props.workingSource, components: allComp });
+        }
+        else if (schema?.type === "table" || schema?.type === "editGrid") {
+
+            let all = []
+
+            for (let x of props?.workingSource?.components) {
+                if (x.key === schema.key) {
+                    all.push(schema)
+                } else {
+                    all.push(x)
+                }
+            }
+
+            props.setWorkingSource({ ...schema, components: all });
+        }
+        else if (schema?.type ==="tabs") {
+            let allComp = [];
+            for (let v of schema?.components) {
+                if (v.type === "file") {
+                    allComp.push({ type: "file", key: v?.key, label: v?.label, storage: "base64", image: true, fileSize: "10240KB" });
+                }
+                else {
+                    allComp.push(v);
+                }
+            }
+
+            let all = []
+
+            for (let x of props?.workingSource?.components) {
+                if (x.key === schema.key) {
+                    all.push({ ...x, components: allComp })
+                } else {
+                    all.push(x)
+                }
+            }
+
+            props.setWorkingSource({ ...schema, components: all });
+        } else {
+
+            let allComp = [];
+            for (let v of schema?.components) {
+                if (v.type === "file") {
+                    allComp.push({ type: "file", key: v.key, label: v.label, storage: "base64", image: true, fileSize: "10240KB" });
+                }
+                else {
+                    allComp.push(v);
+                }
+            }
+
+            props.setWorkingSource({ ...schema, components: allComp });
+
+        }
+
+    };
+
     return (
         <div className="App">
             <FormBuilder
-                form={props.source}
+                form={props.workingSource}
                 onDeleteComponent={(...p) => {
-                    props.onChange(p[1]);
+                    onChange(p[1]);
                 }}
                 onSaveComponent={(...p) => {
                     console.log(p)
-                    props.onChange(p[2]);
+                    onChange(p[2]);
                 }}
                 options={{
                     noDefaultSubmitButton: false
